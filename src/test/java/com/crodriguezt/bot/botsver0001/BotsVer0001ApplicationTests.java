9c1881e3ff9e602dccf7ae52f15f1ab6359c5d07
Cesar Rodriguez Toledo <cesar.2010.11@gmail.com>
2017-12-16 13:17:36 -0500

    Initialize project

---
+++ src/test/java/com/crodriguezt/bot/botsver0001/BotsVer0001ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crodriguezt.bot.botsver0001;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BotsVer0001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
